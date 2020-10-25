c8353666c9c0a58d72f7a82c30456ef36f91bd7d
Denis Popkov <pkvdenis@gmail.com>
2020-10-25 15:12:31 +0300

    fsangsb: ecommproj: backend: springbootecomm: add project

---
+++ fsangsb/ecomm-proj/02-backend/springbootecomm/src/main/java/learn/spr/sh4b/ecomm/SpringbootecommApplication.java
@@ -0,0 +1,13 @@
+package learn.spr.sh4b.ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootecommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootecommApplication.class, args);
+	}
+
+}
